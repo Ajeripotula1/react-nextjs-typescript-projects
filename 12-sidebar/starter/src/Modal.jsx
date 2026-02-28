5dd592d9c1961d1280bee3fb4a979233d38a9500
Akshayraj Jeripotula <akshayraj.jeripotula@gmail.com>
2026-02-28 12:31:56 -0800

    finishing today

--- 12-sidebar/starter/src/Modal.jsx
+++ 12-sidebar/starter/src/Modal.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import { useAppContext } from "./context";
+import { FaTimes } from "react-icons/fa";
 const Modal = () => {
-  return <section className="modal">Modal Content</section>;
+  const { closeModal } = useAppContext();
+  return (
+    <section className="modal-overlay">
+      <div className="modal-container">
+        <button className="modal-close" onClick={closeModal}>
+          <FaTimes />
+        </button>
+        <h3>Modal Content</h3>
+      </div>
+    </section>
+  );
 };
 
 export default Modal;
