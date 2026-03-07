5e1e70a63e63d8f0b57f518ad7cad9db588b8b7b
Akshayraj Jeripotula <akshayraj.jeripotula@gmail.com>
2026-03-06 20:17:24 -0800

    almost done

--- 14-cart/starter/src/reducer.js
+++ 14-cart/starter/src/reducer.js
@@ -9,10 +9,11 @@ import {
 const reducer = (state, action) => {
   const { type, payload } = action;
   switch (type) {
-    case CLEAR_CART:
+    case CLEAR_CART: {
       console.log("cleared cart");
       return { ...state, cart: new Map() };
-    case REMOVE:
+    }
+    case REMOVE: {
       console.log("removing", payload.id);
       // NEVER directly mutate state
       // create new instance and update instead
@@ -22,7 +23,8 @@ const reducer = (state, action) => {
       updatedMap.delete(payload.id);
       // update state
       return { ...state, cart: updatedMap };
-    case INCREASE:
+    }
+    case INCREASE: {
       console.log("increasing amount");
       // create new obj to avoid mutating original
       const newCart = new Map(state.cart);
@@ -37,6 +39,30 @@ const reducer = (state, action) => {
       // console.log("res", state.cart, itemToUpdate);
       // return updated state
       return { ...state, cart: newCart };
+    }
+
+    case DECREASE: {
+      // decrease the items amount
+      console.log("decreasing amount");
+      // create copy of state
+      const newCopy = new Map(state.cart);
+      // get the item data we want to change
+      const itemToDecrease = newCopy.get(payload.id);
+      // check the amount for 0 entry
+      if (itemToDecrease.amount == 1) {
+        // remove the item
+        newCopy.delete(payload.id);
+        return { ...state, cart: newCopy };
+      }
+      // decrement and return
+      const itemToDecreaseUpdated = {
+        ...itemToDecrease,
+        amount: itemToDecrease.amount - 1,
+      };
+      // update the map
+      newCopy.set(payload.id, itemToDecreaseUpdated);
+      return { ...state, cart: newCopy };
+    }
   }
 
   throw new Error("No matching action type:", action.type);
